2ce0b9fb6a5e627d724d76869de01efd200b9854
stela1191 <57503296+stela1191@users.noreply.github.com>
2019-11-21 08:46:43 +0100

    Update klientska_app.py

--- klientska_app.py
+++ klientska_app.py
@@ -43,45 +43,53 @@ def frame2():
     ucty_list = tk.Listbox(root, width=43, height = 8, font='Arial 13',selectmode='SINGLE', xscrollcommand=True)
     ucty_list.insert(1, "BEZNY UCET, SK68 0651 0000 0000 0000 0000")
     ucty_list.insert(2, "FIREMNY UCET, SK68 0651 0000 0000 0000 0000")
-    ucty_list.place(x=20,y=70)
+    ucty_list.place(x=20,y=200)
     
     karty_list = tk.Listbox(root, width=43, height = 8, font='Arial 13',selectmode='SINGLE', xscrollcommand=True)
     karty_list.insert(1, "KREDITNA KARTA, dlh = 120$")
     karty_list.insert(2, "DEBETNA KARTA")
-    karty_list.place(x=(w//3)*2+20,y=70)
+    karty_list.place(x=(w//3)*2+20,y=200)
     
     transakcia_btn=tk.Button(root,text='TRANSAKCIA',command=frame2)
     transakcia_btn.place(width=200,height=25,x=w//6-100,y=430)
     
-    platprik_btn=tk.Button(root,text='PLATOBNY PRIKAZ',command=frame2)
-    platprik_btn.place(width=200,height=25,x=w//6-100,y=480)
+    platprik_btn=tk.Button(root,text='PLATOBNY PRIKAZ',command=platobny_prikaz)
+    platprik_btn.place(width=200,height=25,x=w//6-100,y=470)
     
     prijmy_btn=tk.Button(root,text='PRIJMY',command=frame2)
-    prijmy_btn.place(width=200,height=25,x=w//6-100,y=530)
-    
-    potvrdplatbu_btn=tk.Button(root,text='potvrdit platbu',command=frame2)
-    potvrdplatbu_btn.place(width=200,height=25,x=w//2-100,y=210)
+    prijmy_btn.place(width=200,height=25,x=w//6-100,y=510)
+
+    karty_btn=tk.Button(root,text='KARTY',command=vymaz_pravu_stranu)
+    karty_btn.place(width=200,height=25,x=w//6-100,y=550)
     
     splatdlh_btn=tk.Button(root,text='splatit dlh',command=frame2)
-    splatdlh_btn.place(width=200,height=25,x=(w//6)*5-100,y=240)
+    splatdlh_btn.place(width=200,height=25,x=(w//6)*5-100,y=350)
     
-    prijemca_entry = tk.Entry(root)
-    prijemca_entry.place(width=200,height=25,x=w//2-100,y=100)
-    
-    suma_entry = tk.Entry(root)
-    suma_entry.place(width=200,height=25,x=w//2-100,y=170)
-    
-    can.create_text(w//2,80,text='Prijemca')
-    can.create_text(w//2,150,text='Suma')
-    can.create_text(w//6,40,text='ÚČTY',font='Arial 25')
-    can.create_text(w//2,40,text='PLATOBNY PRIKAZ',font='Arial 25')
-    can.create_text((w//6)*5,40,text='KARTY',font='Arial 25')
+    can.create_text(w//6+30,150,text='ÚČTY',font='Arial 25')
+
+    can.create_text((w//6)*5,150,text='KARTY',font='Arial 25')
     can.create_text(w//6,300,text='zostatok na ucte: 1234$6',font='Arial 20')
 
-##    round_rectangle(400, 200, w-400, h-200, radius=50)
-##    round_rectangle(400, 200, w-400, h-200, radius=50)
-##    round_rectangle(400, 200, w-400, h-200, radius=50)
+
+
+def platobny_prikaz():
+    global plat_prik, potvrdplatbu_btn, prijemca_entry, suma_entry
+    can.create_text(w//2,150,text='PLATOBNY PRIKAZ',font='Arial 25')
     
+    potvrdplatbu_btn=tk.Button(root,text='potvrdit platbu',command=frame2)
+    potvrdplatbu_btn.place(width=200,height=25,x=w//2-100,y=340)
+    
+    prijemca_entry = tk.Entry(root)
+    prijemca_entry.place(width=200,height=25,x=w//2-100,y=210)
+    
+    suma_entry = tk.Entry(root)
+    suma_entry.place(width=200,height=25,x=w//2-100,y=280)
+    
+    can.create_text(w//2,190,text='Prijemca')
+    can.create_text(w//2,260,text='Suma')
+    
+    plat_prik=True
+
 def login():
     global prihlasit_btn, ID_entry
     prihlasit_btn=tk.Button(root,text='BUTONIK',command=frame2)
@@ -92,7 +100,15 @@ def login():
     
     round_rectangle(400, 200, w-400, h-200, radius=50)
     can.create_text(w//2,h//2-20,text='LOGIN',font='Arial 30')
+
+def vymaz_pravu_stranu():
+    global potvrdplatbu_btn, prijemca_entry, suma_entry
+    if plat_prik:
+        can.delete('all')
+        frame2()
+        potvrdplatbu_btn.destroy()
+        prijemca_entry.destroy()
+        suma_entry.destroy()
 login()
 
-#bez frame, vandak style riesenie na widgety :D menej prace, menej errorov...
-#
+
