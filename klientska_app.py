82b95322f8a5afb90f97b2a36f47a3f9711b5415
stela1191 <57503296+stela1191@users.noreply.github.com>
2019-11-27 22:40:06 +0100

    Update klientska_app.py

--- klientska_app.py
+++ klientska_app.py
@@ -35,7 +35,7 @@ def round_rectangle(x1, y1, x2, y2, radius=50, color='black', **kwargs):
     return can.create_polygon(points, **kwargs, smooth=True,fill=color)
 
 def frame2():
-    global prihlasit_btn, ID_entry, ucty_list
+    global prihlasit_btn, ID_entry, ucty_list,karty_btn,transakcia_btn,platprik_btn,potvrdplatbu_btn,prijmy_btn,splatdlh_btn,prijemca_entry,suma_entry,can
     can.delete('all')
     prihlasit_btn.destroy()
     ID_entry.destroy()
@@ -50,7 +50,7 @@ def frame2():
     ucty_list.insert(2, "FIREMNY UCET, SK68 0651 0000 0000 0000 0000")
     ucty_list.place(x=w//2-450,y=200)
     
-    transakcia_btn=tk.Button(root,text='TRANSAKCIA',command=frame2)
+    transakcia_btn=tk.Button(root,text='TRANSAKCIA',command=frame3)
     transakcia_btn.place(width=200,height=25,x=w//2-355,y=410)
     
     platprik_btn=tk.Button(root,text='PLATOBNY PRIKAZ',command=platobny_prikaz_def)
@@ -61,6 +61,37 @@ def frame2():
 
     karty_btn=tk.Button(root,text='KARTY',command=karty_def)
     karty_btn.place(width=200,height=25,x=w//2-355,y=530)
+
+def frame3():
+    global scrollbar, trans_list, spat_btn
+    vymaz_pravu_stranu()
+    vymaz_lavu_stranu()
+    
+    round_rectangle(50, 50, w-50, h-50, radius=50,color='#71CAE7', outline='black',width=3)
+    
+    can.create_text(w//2,75,text='Transakcie', font= 'Arial 25') # CANVAS TEXT zaberie menej miesta = prehladnejsie, menej roboty s tym je 
+
+    scrollbar = tk.Scrollbar(root)
+    scrollbar.place(x=w-120,y=100, height=h-200, width=20)
+    trans_list = tk.Listbox(root, font='Arial 15')
+    trans_list.place(x=100,y=100,width=w-220,height=h-200)
+    for x in range(100):    
+        trans_list.insert(x*3, 'Ucet'+100*' ' +'Ostatok')
+        trans_list.insert(x*3+1, 'Komu')
+        trans_list.insert(x*3+2, '')
+    trans_list.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(command=trans_list.yview)
+
+    spat_btn = tk.Button(root,text='spat',command=spat_def)
+    spat_btn.place(width=200,height=25,x=100,y=60)
+##    transakcia=suma
+##    p=transakcia//10
+##    if transakcia>0:
+##        can.create_rectangle(700,600,800,600-p*10,fill='green')
+##    else:
+##        can.create_rectangle(w-330,h-100,w-230,h-100-20-p,fill='red')
+
+
     
 
 def platobny_prikaz_def():
@@ -156,6 +187,22 @@ def vymaz_pravu_stranu():
         frame2()
         prijmy_list.destroy()
 
+def vymaz_lavu_stranu():
+    global karty_btn, prijmy_btn, platprik_btn, transakcia_btn, ucty_list
+    can.delete('all')
+    karty_btn.destroy()
+    prijmy_btn.destroy()
+    platprik_btn.destroy()
+    transakcia_btn.destroy()
+    ucty_list.destroy()
+
+def spat_def():
+    global scrollbar, trans_list, spat_btn
+    can.delete('all')
+    scrollbar.destroy()
+    trans_list.destroy()
+    spat_btn.destroy()
+    frame2()
 
 # VYSVETLIVKY: tf = true/false, btn = BUTTON
 
@@ -163,3 +210,4 @@ karty_tf=False
 platobny_prikaz_tf=False
 prijmy_tf=False
 login()
+
