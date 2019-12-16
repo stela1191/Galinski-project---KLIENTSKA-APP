e0dc5428f6decba1a30b88f5f715fd97fba36ec8
ChristiH <57683415+ChristiH@users.noreply.github.com>
2019-12-16 14:18:19 +0100

    Update klientska_app.py

--- klientska_app.py
+++ klientska_app.py
@@ -40,17 +40,26 @@ def round_rectangle(x1, y1, x2, y2, radius=50, color='black', **kwargs):
     return can.create_polygon(points, **kwargs, smooth=True,fill=color)
 
 def frame2():
-    global prihlasit_btn, ID_entry, ucty_list,karty_btn,transakcia_btn,platprik_btn,potvrdplatbu_btn,prijmy_btn,splatdlh_btn,prijemca_entry,suma_entry,can, odhlasenie_btn, z_loginu
+    global prihlasit_btn, ID_entry, ucty_list,karty_btn,transakcia_btn,platprik_btn,potvrdplatbu_btn,prijmy_btn,splatdlh_btn,prijemca_entry,suma_entry,can, odhlasenie_btn, z_loginu,id_uctu, id_klienta,cislo_uctu, stav_uctu
 
     round_rectangle(100, 100, w-100, h-100, radius=50,color='#71CAE7', outline='black',width=3)
+
+    kon_ucty()
+    citaj_ucty()
     
     can.create_text(w//2-255,150,text='ÚČTY',font='Arial 25')
     can.create_text(w//2-255,300,text='zostatok na ucte: 1234$6',font='Arial 20')
     
     ucty_list = tk.Listbox(root, width=43, height = 8, font='Arial 13',selectmode='SINGLE', xscrollcommand=True)
-    ucty_list.insert(1, "BEZNY UCET, SK68 0651 0000 0000 0000 0000")
-    ucty_list.insert(2, "FIREMNY UCET, SK68 0651 0000 0000 0000 0000")
+    #ucty_list.insert(1, 'BEZNY UCET',str(cislo_uctu))
+    #ucty_list.insert(2, 'FIREMNY UCET',str(cislo_uctu))
     ucty_list.place(x=w//2-450,y=200)
+    poradie = 0
+    for u in range(2):
+        if prihlaseny_ID == id_klienta[u]:
+            poradie+=1
+            ucty_list.insert(poradie, 'BEZNY UCET'+20*' '+'SK '+cislo_uctu[u])
+            
     
     transakcia_btn=tk.Button(root,text='TRANSAKCIA',command=frame3)
     transakcia_btn.place(width=200,height=25,x=w//2-355,y=410)
@@ -85,8 +94,8 @@ def frame3():
     scrollbar.place(x=w-120,y=100, height=h-200, width=20)
     trans_list = tk.Listbox(root, font='Arial 15')
     trans_list.place(x=100,y=100,width=w-220,height=h-200)
-    for x in range(100):    
-        trans_list.insert(x*3, 'Ucet'+100*' ' +'Ostatok')
+    for t in range(len(transakcie_ucty)):    
+        trans_list.insert(x*3, 'Ucet'+100*' ' +'Suma')
         trans_list.insert(x*3+1, 'Komu')
         trans_list.insert(x*3+2, '')
     trans_list.config(yscrollcommand=scrollbar.set)
@@ -94,14 +103,7 @@ def frame3():
 
     spat_btn = tk.Button(root,text='SPÄŤ',command=spat_def)
     spat_btn.place(width=200,height=25,x=100,y=60)
-##    transakcia=suma
-##    p=transakcia//10
-##    if transakcia>0:
-##        can.create_rectangle(700,600,800,600-p*10,fill='green')
-##    else:
-##        can.create_rectangle(w-330,h-100,w-230,h-100-20-p,fill='red')
 
-    
 
 def platobny_prikaz_def():
     global platobny_prikaz_tf, karty_tf, prijmy_tf, potvrdplatbu_btn, prijemca_entry, suma_entry
@@ -153,18 +155,16 @@ def karty_def():
                 karty_list.insert(poradie, typ_k)
             else:
                 typ_k = 'KREDITNÁ KARTA, '
-                karty_list.insert(poradie, typ_k + 'dlh na karte: ' + str(dlh_karty[h]))
+                karty_list.insert(poradie, typ_k + 20*' '+'dlh na karte: ' + str(dlh_karty[h]))
+                suma2_entry = tk.Entry()
+                suma2_entry.pack()
+                suma2_entry.place(width=200,height=25,x=w//2+155,y=h-300)
+                can.create_text(w//2+255, h-130, text='Zadajte sumu', font='Arial 15')
+                splatdlh_btn=tk.Button(root,text='SPLATIT DLH',command=splatit)
+                splatdlh_btn.place(width=200,height=25,x=w//2+155,y=h-250)
+                
     karty_list.place(x=w//2+65,y=200)
-    
     can.create_text(w//2+255,150,text='KARTY',font='Arial 25')
-    
-    suma2_entry = tk.Entry()
-    suma2_entry.pack()
-    suma2_entry.place(width=200,height=25,x=w//2+155,y=h-300)
-
-    can.create_text(w//2+255, h-330, text='Zadajte sumu', font='Arial 15')
-    splatdlh_btn=tk.Button(root,text='SPLATIT DLH',command=splatit)
-    splatdlh_btn.place(width=200,height=25,x=w//2+155,y=h-250)
   
     karty_tf=True
     platobny_prikaz_tf=False
@@ -363,9 +363,78 @@ def citaj_karty():
         os.remove("KARTY_LOCK.txt")
     else:
         pocet=0
+
+def kon_ucty():
+    global lock_ucty
+    if not os.path.exists('UCTY_LOCK.txt'):
+        lock_ucty = False
+    else:
+        lock_ucty = True
+
+
+def citaj_ucty():
+    global id_uctu, id_klienta,cislo_uctu, stav_uctu
+    if not lock_ucty:
+        lock_subor = open('UCTY_LOCK.txt','w')
+        subor = open('UCTY.txt','r')
+        pocet = int(subor.readline().strip())
+        id_uctu = []
+        id_klienta = []
+        cislo_uctu = []
+        stav_uctu = []
+        pocet_uctov=0
+        for r in range(pocet): 
+            riadok = subor.readline().strip()
+            k=riadok.split(';')
+            id_uctu.append(k[0])
+            id_klienta.append(k[1])
+            cislo_uctu.append(k[2])
+            stav_uctu.append(k[4])
+            print(id_klienta)
+        
+        subor.close()
+        lock_subor.close()
+        os.remove("UCTY_LOCK.txt")
+    else:
+        pocet=0
+
+
+##def kon_transakcie_ucty():
+##    global lock_transakcie_ucty
+##    if not os.path.exists('TRANSAKCIE_UCTY_LOCK.txt'):
+##        lock_transakcie_ucty = False
+##    else:
+##        lock_transakcie_ucty = True
+##
+##
+##def citaj_transakcie_ucty():
+##    global id_uctu, id_klienta,suma,id_transakcie
+##    if not lock_karty:
+##        lock_subor = open('TRANSAKCIE_UCTY_LOCK.txt','w')
+##        subor = open('TRANSAKCIE_UCTY.txt','r')
+##        pocet = int(subor.readline().strip())
+##        id_uctu = []
+##        id_klienta = []
+##        suma = []
+##        id_transakcie = []
+##        for r in range(pocet): 
+##            riadok = subor.readline().strip()
+##            k=riadok.split(';')
+##            id_uctu.append(k[4])
+##            id_klienta.append(k[3])
+##            suma.append(k5])
+##            id_transakcie.append(k[0])
+##        subor.close()
+##        lock_subor.close()
+##        os.remove("KARTY_LOCK.txt")
+##    else:
+##        pocet=0
         
 ##////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////           
-# DOROBIT TREBA SPLATENIE DLHU, KARTY SU FUNKCNE 
+# DOROBIT TREBA SPLATENIE DLHU-neviem preco to nefunguje
+# nastane problem s uctami ked niekto bude mat viac uctov
+# a v transakciach neviem ze z ktoreho suboru mam citat
+#inac UCTY sa uz daju citat
 prihlaseny_ID = 1
 
 ID_klientov=[]
